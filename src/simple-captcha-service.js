7759ab82b346663a2be57b46fd71a18fc723e6a1
Trigex <trigexss@gmail.com>
2019-03-27 11:53:28 -0700

    cors

--- src/simple-captcha-service.js
+++ src/simple-captcha-service.js
@@ -2,8 +2,16 @@ const config = require('../config.json');
 const restify = require('restify');
 const pino = require('./utils/pino');
 const routes = require('./routes/index.routes');
+const corsMiddleware = require('restify-cors-middleware');
 
 const server = restify.createServer({name: 'Simple Captcha Service'});
+const cors = corsMiddleware({
+    origins: ['*'],
+    allowHeaders: ['Content-Type', 'Accept', 'Access-Control-Allow-Headers']
+});
+server.pre(cors.preflight);
+server.use(cors.actual);
+
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.throttle({
     burst: config.server.throttling.burst,
